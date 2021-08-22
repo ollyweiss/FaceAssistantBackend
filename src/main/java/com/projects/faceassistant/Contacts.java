2b6eb459fd2603ffe1557c10922a8973b14c9f94
ollyweiss <olly.weiss@gmail.com>
2021-08-22 14:40:17 -0400

    created db table contacts

--- src/main/java/com/projects/faceassistant/Contacts.java
+++ src/main/java/com/projects/faceassistant/Contacts.java
@@ -4,10 +4,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloController {
+public class Contacts {
 
-    @GetMapping("/")
-    public String test() {
+    @GetMapping("/contacts")
+    public String getContacts() {
         return "Hello world";
     }
 }
