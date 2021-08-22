d6236c5e93ef024b4a61e356cf31c5d7c73eab47
ollyweiss <olly.weiss@gmail.com>
2021-08-22 13:52:03 -0400

    init

---
+++ src/main/java/com/projects/faceassistant/FaceAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.projects.faceassistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaceAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaceAssistantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projects/faceassistant/FaceAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.faceassistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaceAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
